2a5041475b3595e45f60439120073c0d81ba948b
hanzala434 <hanzalay959@gmail.com>
2024-09-27 17:13:51 +0500

    Initial commit

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import JobListings from '../components/JobListings'
+
+const JobsPage = () => {
+  return (
+<section className='bg-blue-50 px-2 py-6'>
+    <JobListings/>
+</section>
+  )
+}
+
+export default JobsPage
